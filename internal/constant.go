d5db42120ea9feeb2d9e51cd856a3f2be1b52380
Jeremiah <jmiller.v@gmail.com>
2020-04-20 13:03:39 -0600

    corpopts: update versioning, remove help text

--- internal/constant.go
+++ internal/constant.go
@@ -2,5 +2,5 @@ package internal
 
 const APP_NAME = "Go Phrase"
 const APP_USAGE = "CLI for generating secure, memorable passwords"
-const APP_VERSION = "v0.0.1 | release: 00.4.18.20"
+const APP_VERSION = "v0.0.3 | release: 00.4.20.20"
 const APP_USAGETEXT = "gophrase <int>"
