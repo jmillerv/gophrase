82c692acf5ebad27ff5e56913976f0330141565a
Jeremiah <jmiller.v@gmail.com>
2020-04-20 19:58:04 -0600

    validation: update usage text constant

--- internal/constant.go
+++ internal/constant.go
@@ -4,7 +4,7 @@ package internal
 const APP_NAME = "Go Phrase"
 const APP_USAGE = "CLI for generating secure, memorable passwords"
 const APP_VERSION = "v0.0.3 | release: 00.4.20.20"
-const APP_USAGETEXT = "gophrase <int>"
+const APP_USAGETEXT = "gophrase gen <word count> <word list>"
 
 // Asset location constants
 const EFF_LARGE = "eff_large_wordlist.json"
