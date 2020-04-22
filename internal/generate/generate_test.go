26f9673230aa125c562df388e591cdd37a530a47
Jeremiah <jmiller.v@gmail.com>
2020-04-21 21:26:21 -0600

    testing: add TestKeySize

--- internal/generate/generate_test.go
+++ internal/generate/generate_test.go
@@ -1,1 +1,20 @@
 package generate
+
+import "testing"
+
+func TestKeySize (t *testing.T){
+	type tables struct {
+		first string
+		second string
+	}
+
+	var testValues = tables{"a", "d"}
+
+	if keySize(testValues.first) != 4 {
+		t.Errorf("keySize() failed expected 4, got %v", keySize(testValues.first))
+	}
+
+	if keySize(testValues.second) != 5 {
+		t.Errorf("keySize() failed expected 5, got %v", keySize(testValues.second))
+	}
+}
