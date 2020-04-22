f8e4e88ee0e46d8d694c063c39ed7b8cd04057ed
Jeremiah <jmiller.v@gmail.com>
2020-04-21 21:13:28 -0600

    testing: add TestGetWordList

--- internal/corpus/corpus_test.go
+++ internal/corpus/corpus_test.go
@@ -1,1 +1,42 @@
 package corpus
+
+import (
+	"github.com/gophrase/internal"
+	"testing"
+)
+
+func TestGetWordList(t *testing.T) {
+	type tables struct {
+		first  string
+		second string
+		third  string
+		fourth string
+		fifth string
+	}
+
+	var testValues = tables{"a", "b", "c", "d", ""}
+
+	if  setWordList(testValues.first) != internal.EFF_SHORT_2 {
+		t.Errorf("setWordList() failed expected 'eff_short_wordlist_2_0.json', but got %s", setWordList(testValues.first))
+	}
+	if  setWordList(testValues.second) != internal.EFF_SHORT_1 {
+		t.Errorf("setWordList() failed expected 'eff_short_wordlist_1.json', but got %s", setWordList(testValues.second))
+	}
+	if  setWordList(testValues.third) != internal.EFF_LARGE {
+		t.Errorf("setWordList() failed expected 'eff_large_wordlist.json', but got %s", setWordList(testValues.third))
+	}
+	if  setWordList(testValues.fourth) != internal.REINHOLD {
+		t.Errorf("setWordList() failed expected 'reinhold_wordlist.json', but got %s", setWordList(testValues.fourth))
+	}
+	if  setWordList(testValues.fifth) != internal.EFF_SHORT_2 {
+		t.Errorf("setWordList() failed expected 'eff_short_wordlist_2_0.json', but got %s", setWordList(testValues.fifth))
+	}
+}
+
+func testGetWord(t *testing.T) {
+
+}
+
+func testSetWordList(t *testing.T) {
+
+}
