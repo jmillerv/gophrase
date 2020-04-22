c28e15a92e268755ddb59cc3b9995c50f399aaa4
Jeremiah <jmiller.v@gmail.com>
2020-04-21 21:42:52 -0600

    testing: add TestPassword

--- internal/generate/generate_test.go
+++ internal/generate/generate_test.go
@@ -1,6 +1,9 @@
 package generate
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestKeySize (t *testing.T){
 	type tables struct {
@@ -17,4 +20,35 @@ func TestKeySize (t *testing.T){
 	if keySize(testValues.second) != 5 {
 		t.Errorf("keySize() failed expected 5, got %v", keySize(testValues.second))
 	}
+}
+
+func TestKey (t *testing.T){
+	type tables struct {
+		first string
+		second string
+	}
+
+	var testValues = tables{"a", "d"}
+	key1 := key(testValues.first)
+	// check if key is 4 digit number
+	if getDigit(key1, 4) == 0 && getDigit(key1, 5) == 0 {
+		t.Errorf("key() failed expected key with 4 digits")
+	}
+	// check if key is 5 digit number
+	key2 := key(testValues.second)
+	if getDigit(key2, 5) == 0 {
+		t.Errorf("key() failed expected key with 5 digits")
+	}
+}
+
+func TestPassword(t *testing.T) {
+	passphrase := Password(3, "d")
+	if len(passphrase) <= 0 {
+		t.Errorf("Password() failed expected string, got empty return")
+	}
+}
+
+func getDigit(num, length int) int {
+	r := num % int(math.Pow(10, float64(length)))
+	return r / int(math.Pow(10, float64(length-1)))
 }
