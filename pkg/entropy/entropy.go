122a3a4fdd6278601b6bc2673bde6a3c7d799532
Jeremiah <jmiller.v@gmail.com>
2020-05-10 13:02:07 -0600

    licenses: formatting

--- pkg/entropy/entropy.go
+++ pkg/entropy/entropy.go
@@ -8,10 +8,10 @@ import (
 // Initially I wrote my own entropy calculator but found this package that's based on a Dropbox
 // it's more conservative in its estimation than my original and those I found online. So, it's included here.
 
-func PrintEntropy(passphrase string ) {
-	entropy := zxcvbn.PasswordStrength(passphrase,nil)
+func PrintEntropy(passphrase string) {
+	entropy := zxcvbn.PasswordStrength(passphrase, nil)
 	fmt.Println("Password: " + entropy.Password)
-	fmt.Println("Entropy: ", entropy.Entropy)
+	fmt.Println("Entropy: ", entropy.Entropy, "bits")
 	fmt.Println("Crack Time: " + entropy.CrackTimeDisplay)
 	fmt.Println("Score: ", entropy.Score, "/ 4")
 }
