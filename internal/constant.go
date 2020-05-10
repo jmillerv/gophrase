b422f0e03aff08f7b85e83db1a5a4817e58cae3c
Jeremiah <jmiller.v@gmail.com>
2020-05-09 22:55:13 -0600

    list: update constants for file locations

--- internal/constant.go
+++ internal/constant.go
@@ -3,7 +3,7 @@ package internal
 // App information constants
 const APP_NAME = "Go Phrase"
 const APP_USAGE = "CLI for generating secure, memorable passwords"
-const APP_VERSION = "v0.0.5 | release: 00.4.25.20"
+const APP_VERSION = "v0.0.6 | release: 00.5.9.20"
 const APP_USAGETEXT = "gophrase gen [word count] [word list] [--flags]"
 
 // Static asset constants
@@ -12,3 +12,4 @@ const EFF_SHORT_1 = "eff_short_wordlist_1.json"
 const EFF_SHORT_2 = "eff_short_wordlist_2_0.json"
 const REINHOLD = "reinhold_wordlist.json"
 const CHARACTERS = "special_characters.json"
+const LIST_OPTIONS = "list_options.txt"
