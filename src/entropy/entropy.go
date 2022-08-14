d507a87545f9685396674b8f17b3a4825116490a
Jeremiah <jmiller.v@gmail.com>
2022-08-14 15:58:40 -0400

    entropy/entrpoy: implement PrintEntropy

--- src/entropy/entropy.go
+++ src/entropy/entropy.go
@@ -5,9 +5,10 @@ import (
 	"github.com/nbutton23/zxcvbn-go"
 )
 
-// Initially I wrote my own entropy calculator but found this package that's based on a Dropbox
+// Initially I wrote my own entropy calculator but found this package that's based on a Dropbox package
 // it's more conservative in its estimation than my original and those I found online. So, it's included here.
 
+// PrintEntropy returns the metadata about the strenght of a given passphrase
 func PrintEntropy(passphrase string) {
 	entropy := zxcvbn.PasswordStrength(passphrase, nil)
 	fmt.Println("Password: " + entropy.Password)
