5817fdb04375db4dd73646c1d2bd5a04494e9139
Jeremiah <jmiller.v@gmail.com>
2022-08-20 18:20:52 -0400

    feat: implement GetEntropy

--- src/entropy/entropy.go
+++ src/entropy/entropy.go
@@ -3,16 +3,34 @@ package entropy
 import (
 	"fmt"
 	"github.com/nbutton23/zxcvbn-go"
+	"github.com/nbutton23/zxcvbn-go/scoring"
 )
 
-// Initially I wrote my own entropy calculator but found this package that's based on a Dropbox package
-// it's more conservative in its estimation than my original and those I found online. So, it's included here.
+type Metadata struct {
+	Entropy   float64 `json:"entropy,omitempty"`
+	CrackTime string  `json:"crack_time,omitempty"`
+	Score     int     `json:"score,omitempty"`
+}
 
-// PrintEntropy returns the metadata about the strenght of a given passphrase
+// PrintEntropy returns the password and metadata about the strength of a given passphrase to STD out
 func PrintEntropy(passphrase string) {
-	entropy := zxcvbn.PasswordStrength(passphrase, nil)
+	entropy := getEntropy(passphrase)
 	fmt.Println("Password: " + entropy.Password)
 	fmt.Println("Entropy: ", entropy.Entropy, "bits")
 	fmt.Println("Crack Time: " + entropy.CrackTimeDisplay)
 	fmt.Println("Score: ", entropy.Score, "/ 4")
+}
+
+// GetEntropy returns the Metadata about the password
+func GetEntropy(passphrase string) *Metadata {
+	entropy := getEntropy(passphrase)
+	return &Metadata{
+		Entropy:   entropy.Entropy,
+		CrackTime: entropy.CrackTimeDisplay,
+		Score:     entropy.Score,
+	}
+}
+
+func getEntropy(passphrase string) scoring.MinEntropyMatch {
+	return zxcvbn.PasswordStrength(passphrase, nil)
 }
