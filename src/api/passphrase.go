9a69a48dac91e544fbbed97f30c031aa4551cec7
Jeremiah <jmiller.v@gmail.com>
2022-08-20 18:30:31 -0400

    feat: add metadata to passphrase endpoint

--- src/api/passphrase.go
+++ src/api/passphrase.go
@@ -3,15 +3,25 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jmillerv/gophrase/corpus"
+	"github.com/jmillerv/gophrase/entropy"
 	"github.com/jmillerv/gophrase/generate"
 	"net/http"
 	"strconv"
 )
 
+type response struct {
+	Passphrase string            `json:"passphrase"`
+	Metadata   *entropy.Metadata `json:"metadata"`
+}
+
 func getPassphrase(c *gin.Context) {
 	params := httpParamsToStruct(c)
 	phrase := generate.Password(params)
-	c.String(http.StatusOK, phrase)
+	metadata := entropy.GetEntropy(phrase)
+	c.JSON(http.StatusOK, &response{
+		Passphrase: phrase,
+		Metadata:   metadata,
+	})
 }
 
 func httpParamsToStruct(c *gin.Context) *generate.Params {
