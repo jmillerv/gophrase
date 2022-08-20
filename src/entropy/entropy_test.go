e605af03500fb249782d5b2a6a39eae7bc8cc4e1
Jeremiah <jmiller.v@gmail.com>
2022-08-20 18:19:24 -0400

    test: update tests

--- src/entropy/entropy_test.go
+++ src/entropy/entropy_test.go
@@ -1,14 +1,58 @@
 package entropy
 
 import (
+	"reflect"
 	"testing"
 )
 
+func TestGetEntropy(t *testing.T) {
+	type args struct {
+		passphrase string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Metadata
+	}{
+		{
+			name: "Success: Returns Expected Metadata",
+			args: args{
+				passphrase: "craveprizemoist",
+			},
+			want: &Metadata{
+				Entropy:   36.21,
+				CrackTime: "3.0 months",
+				Score:     3,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetEntropy(tt.args.passphrase); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetEntropy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestPrintEntropy(t *testing.T) {
-	passphrase := "vaNquished68eMperor9squirrelGetawaY/rustproof284564+"
-	passphrase2 := "lipstickunflavoredgleefuljeepfondue"
-	passphrase3 := "escalatorought"
-	PrintEntropy(passphrase)
-	PrintEntropy(passphrase2)
-	PrintEntropy(passphrase3)
+	type args struct {
+		passphrase string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "",
+			args: args{
+				passphrase: "vaNquished68eMperor9squirrelGetawaY/rustproof284564+",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			PrintEntropy(tt.args.passphrase)
+		})
+	}
 }
