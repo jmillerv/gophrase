bab67a0f0059d7a1661753d899d1eb9344e80059
Jeremiah <jmiller.v@gmail.com>
2022-08-20 18:16:29 -0400

    feat: implement /entropy endpoint

--- src/api/entropy.go
+++ src/api/entropy.go
@@ -1,1 +1,22 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jmillerv/gophrase/entropy"
+	"net/http"
+)
+
+type submission struct {
+	Phrase string `json:"passphrase"`
+}
+
+func postEntropy(c *gin.Context) {
+	var submit submission
+	err := c.BindJSON(&submit)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "improper json body")
+	}
+	metadata := entropy.GetEntropy(submit.Phrase)
+
+	c.JSON(http.StatusOK, &metadata)
+}
