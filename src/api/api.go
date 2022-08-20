9e628c8d68f8ad3df5bf22a1611d70486a4acf95
Jeremiah <jmiller.v@gmail.com>
2022-08-20 18:16:10 -0400

    feat: implement Serve()

--- src/api/api.go
+++ src/api/api.go
@@ -1,1 +1,19 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func Serve() {
+	router := gin.Default()
+
+	router.POST("/entropy", postEntropy)
+	router.GET("options", getOptions)
+	router.GET("/passphrase", getPassphrase)
+
+	err := router.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
